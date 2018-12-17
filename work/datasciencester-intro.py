2e5c104e03100d76bee0f6b42cdc15a2aeb95785
Harish Kamath <development@harishkamath.com>
2018-12-17 15:14:53 +0000

    Included all the examples from the chapter into GH - looks like local copy from home was not uploaded to GH.

--- work/datasciencester-intro.py
+++ work/datasciencester-intro.py
@@ -5,13 +5,14 @@ All the code from the Data Science from Scratch book
 Chapter 1. Introduction
 
 """
-# [] = list
-# () = tuple
-# {} = dict
+# [] = list - mutable
+# () = tuple - immutable
+# {} = dict - hashes (key - value pairs)
 # ?? = set - a dict with unique values
 
 from __future__ import division                   # integer division is lame
 from collections import Counter
+from collections import defaultdict
 
 users = [
     { "id": 0, "name": "Hero" },
@@ -26,12 +27,12 @@ users = [
     { "id": 9, "name": "Klein" }
 ]
 
-#print(users)
+# print(users)
 
 friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
-print(friendships)
+# print(friendships)
 
 for user in users:
     user["friends"] = []
@@ -49,15 +50,15 @@ def number_of_friends(user):
     return len(user["friends"]) # length of friend_ids list
 
 connections = [number_of_friends(user) for user in users]
-#print("Connections (v1):", connections)
+print("Connections (v1):", connections)
 
 connections = [(user["id"], number_of_friends(user))
                 for user in users]
-#print("Connections (v2):", connections)
+print("Connections (v2):", connections)
 
 total_connections = sum(number_of_friends(user)                        
                                  for user in users) # 24
-#print("Total connections: ", total_connections)
+print("Total connections: ", total_connections)
 
 
 num_users = len(users)                            # length of the users list
@@ -85,8 +86,8 @@ print(sorted(num_friends_by_id, # get it sorted
 ## def double(x):
 ##    return x * 2
 ## Program to show the use of lambda functions
-#double = lambda x: x * 2
-#print(double(5)) # Output: 10
+# double = lambda x: x * 2
+# print(double(5)) # Output: 10
 #
 #
 #
@@ -143,4 +144,140 @@ def friends_of_friend_ids(user):
     
 #print(friends_of_friend_ids(users[3]))
 
-                    
+
+interests = [    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),    (3, "statistics"), (3, "regression"), (3, "probability"),    (4, "machine learning"), (4, "regression"), (4, "decision trees"),    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),    (6, "probability"), (6, "mathematics"), (6, "theory"),    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),    (9, "Java"), (9, "MapReduce"), (9, "Big Data")]
+
+
+def data_scientists_who_like(target_interest):
+    return [user_id
+            for user_id, user_interest in interests
+            if user_interest == target_interest]
+            
+print data_scientists_who_like('Hadoop')            
+
+
+# keys are interests, values are lists of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+    
+
+print user_ids_by_interest['Java']
+
+# keys are user_ids, values are lists of interests for that user_id
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+    
+print interests_by_user_id[2]
+
+
+def most_common_interests_with(user):
+    return Counter(interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"])
+        
+print(most_common_interests_with(users[2]))        
+
+
+salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
+                        (48000, 0.7), (76000, 6),
+                        (69000, 6.5), (76000, 7.5),
+                        (60000, 2.5), (83000, 10),
+                        (48000, 1.9), (63000, 4.2)]
+                        
+# keys are years, values are lists of the salaries for each tenure
+salary_by_tenure = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    salary_by_tenure[tenure].append(salary)
+
+# keys are years, each value is average salary for that tenure
+average_salary_by_tenure = {
+    tenure : sum(salaries) / len(salaries)
+    for tenure, salaries in salary_by_tenure.items()
+}                        
+
+print(average_salary_by_tenure)
+
+def tenure_bucket(tenure):
+    if tenure < 2:
+        return "less than two"
+    elif tenure < 5:
+        return "between two and five"
+    else:
+        return "more than five"
+        
+# keys are tenure buckets, values are lists of salaries for that bucket
+salary_by_tenure_bucket = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    bucket = tenure_bucket(tenure)
+    salary_by_tenure_bucket[bucket].append(salary)
+    
+print(salary_by_tenure_bucket)    
+
+# keys are tenure buckets, values are average salary for that bucket
+average_salary_by_bucket = {
+  tenure_bucket : sum(salaries) / len(salaries)
+  for tenure_bucket, salaries in salary_by_tenure_bucket.iteritems()
+}
+
+
+print(average_salary_by_bucket)
+
+
+
+# 0.7 paid
+# 1.9 unpaid
+# 2.5 paid
+# 4.2 unpaid
+# 6   unpaid
+# 6.5 unpaid
+# 7.5 unpaid
+# 8.1 unpaid
+# 8.7 paid
+# 10  paid
+
+def predict_paid_or_unpaid(years_experience):
+  if years_experience < 3.0:
+    return "paid"
+  elif years_experience < 8.5:
+    return "unpaid"
+  else:
+    return "paid"
+    
+
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+
+words_and_counts = Counter(word
+                           for user, interest in interests
+                           for word in interest.lower().split())
+                           
+print(words_and_counts)                           
+
+for word, count in words_and_counts.most_common():
+    if count > 1:
+        print word, count
+        
+        
